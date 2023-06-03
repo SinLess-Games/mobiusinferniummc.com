ec532017eec071587038611db22153c519edbb40
Timothy Pierce <disdainful777@gmail.com>
2023-06-02 22:55:34 -0600

    Update packages

--- src/pages/api/v1/auth/index.ts
+++ src/pages/api/v1/auth/index.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req:NextApiRequest, res: NextApiResponse ) {
+export default function handler(req: NextApiRequest, res: NextApiResponse ) {
     res.status( 200 ).json({
         ApiVersion: '1.0.0',
         Owner: 'SinLess Games LLC',
